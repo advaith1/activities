cbbbbd8472bebd7c345e814dea57603aaca1b7f4
Advaith <advaithj1@gmail.com>
2021-04-17 18:39:29 -0700

    Discord now requires including content-type
    also update deps & clean up code

--- src/bot.ts
+++ src/bot.ts
@@ -1,43 +1,47 @@
 import { verifyKey } from 'discord-interactions'
-import { InteractionType, APIInteractionResponseType, APIInteractionResponse, APIInteraction, Snowflake, RESTPostAPIChannelInviteJSONBody, APIInvite } from 'discord-api-types/v8'
+import { InteractionType, InteractionResponseType, APIInteractionResponse, RESTPostAPIChannelInviteJSONBody, APIInvite, APIApplicationCommandGuildInteraction, ApplicationCommandOptionType, ChannelType, MessageFlags, InviteTargetUserType } from 'discord-api-types/v8'
 
 export async function handleRequest(request: Request): Promise<Response> {
   if (!request.headers.get('X-Signature-Ed25519') || !request.headers.get('X-Signature-Timestamp')) return Response.redirect('https://advaith.io')
   const valid = verifyKey(await request.clone().arrayBuffer(), request.headers.get('X-Signature-Ed25519')!, request.headers.get('X-Signature-Timestamp')!, publicKey)
   if (!valid) return new Response('', { status: 401 })
 
-  const interaction = await request.json() as APIInteraction
+  const interaction = await request.json() as APIApplicationCommandGuildInteraction
 
   if (interaction.type === InteractionType.Ping)
     return respond({
-      type: APIInteractionResponseType.Pong
+      type: InteractionResponseType.Pong
     })
 
-  if (!interaction.data!.resolved)
+  if (!interaction.data.resolved?.channels)
     return respond({
-      type: APIInteractionResponseType.ChannelMessageWithSource,
+      type: InteractionResponseType.ChannelMessageWithSource,
       data: {
         content: 'Please update your Discord app to use this command.',
-        flags: 1 << 6
+        flags: MessageFlags.EPHEMERAL
       }
     })
 
-  // @ts-expect-error / ts doesn't like this for some reason
-  if (interaction.data.resolved.channels[interaction.data.options[0].value as Snowflake].type !== 2)
+  // set option types for ts
+  if (!interaction.data.options ||
+      interaction.data.options[0].type !== ApplicationCommandOptionType.CHANNEL ||
+      interaction.data.options[1].type !== ApplicationCommandOptionType.STRING) return new Response()
+
+  if (interaction.data.resolved.channels[interaction.data.options[0].value].type !== ChannelType.GUILD_VOICE)
     return respond({
-      type: APIInteractionResponseType.ChannelMessageWithSource,
+      type: InteractionResponseType.ChannelMessageWithSource,
       data: {
         content: 'The selected channel must be a voice channel'
       }
     })
     
-  const r = await fetch(`https://discord.com/api/v8/channels/${interaction.data!.options![0].value}/invites`, {
+  const r = await fetch(`https://discord.com/api/v8/channels/${interaction.data.options[0].value}/invites`, {
     method: 'POST',
     headers: { authorization: `Bot ${token}`, 'content-type': 'application/json' },
     body: JSON.stringify({
       max_age: 0,
-      target_type: 2,
-      target_application_id: interaction.data!.options![1].value
+      target_type: InviteTargetUserType.EMBEDDED_APPLICATION,
+      target_application_id: interaction.data.options[1].value
     } as RESTPostAPIChannelInviteJSONBody)
   })
 
@@ -45,7 +49,7 @@ export async function handleRequest(request: Request): Promise<Response> {
 
   if (r.status !== 200) {
     return respond({
-      type: APIInteractionResponseType.ChannelMessageWithSource,
+      type: InteractionResponseType.ChannelMessageWithSource,
       data: {
         content: `An error occured: ${(invite as any).message}\nMake sure I have the "Create Invite" permission in the voice channel!`,
         allowed_mentions: { parse: [] }
@@ -54,13 +58,13 @@ export async function handleRequest(request: Request): Promise<Response> {
   }
 
   return respond({
-    type: APIInteractionResponseType.ChannelMessageWithSource,
+    type: InteractionResponseType.ChannelMessageWithSource,
     data: {
-      content: `[Click to open ${invite.target_application.name} in ${invite.channel!.name}](<https://discord.gg/${invite.code}>)`,
+      content: `[Click to open ${invite.target_application!.name} in ${invite.channel!.name}](<https://discord.gg/${invite.code}>)`,
       allowed_mentions: { parse: [] }
     }
   })
 }
 
 const respond = (response: APIInteractionResponse) =>
-  new Response(JSON.stringify(response))
+  new Response(JSON.stringify(response), {headers: {'content-type': 'application/json'}})
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,29 +1,3 @@
-import type { Permissions, Snowflake, APIGuildMember, APIPartialChannel, APIRole, APIUser, APIApplication } from 'discord-api-types/v8';
-
 // secrets: wrangler secret put <name>
-declare global {
-    const publicKey: string
-    const token: string
-}
-
-declare module 'discord-api-types/v8' {
-    // https://github.com/discordjs/discord-api-types/pull/86
-    export interface APIApplicationCommandInteractionData {
-        resolved?: {
-            users?: Record<Snowflake, APIUser>;
-            roles?: Record<Snowflake, APIRole>;
-            members?: Record<Snowflake, Omit<APIGuildMember, 'user' | 'deaf' | 'mute'>>;
-            channels?: Record<Snowflake, Required<APIPartialChannel> & { permissions: Permissions }>;
-        };
-    }
-
-    // https://github.com/discord/discord-api-docs/pull/2690
-    export interface APIInvite {
-        target_type: 1 | 2
-        target_application: APIApplication
-    }
-    export interface RESTPostAPIChannelInviteJSONBody {
-        target_type: 1 | 2
-        target_application_id: Snowflake
-    }
-}
+declare const publicKey: string
+declare const token: string
